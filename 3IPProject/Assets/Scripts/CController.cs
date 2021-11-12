f0405c96717fa3fa013bef4caede9f2e2e8aa946
MiraLyster <miralyster@yahoo.com>
2021-11-12 13:34:25 +0100

    Flipping, removed jump, implemented the doggo with animation

--- 3IPProject/Assets/Scripts/CController.cs
+++ 3IPProject/Assets/Scripts/CController.cs
@@ -4,13 +4,16 @@ public class CController : MonoBehaviour
 {
     public Animator animator;
     public float movementSpeed = 1;
-    public float jumpForce = 1;
-    
+    public float jumpForce = 10;
+   // private bool jump = false;
+    private SpriteRenderer _spriteRenderer;
     private Rigidbody2D _rigidbody;
+    //public GameObject _rayCast;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -20,12 +23,49 @@ public class CController : MonoBehaviour
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
         animator.SetFloat("Speed", Mathf.Abs(movement));
+        if (movement < 0)
+        {
+            _spriteRenderer.flipX = true;
+        } 
+        else if(movement > 0){
+            _spriteRenderer.flipX = false;
+        }
 
+        /*
         if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
         {
+            
+            //if (jump == false) jump = true;
             _rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             animator.SetBool("IsJumping", true);
+            
+            //Jump flag
         }
-       
+        /*
+        if (animator.GetBool("IsJumping") && !jump)
+        {
+            animator.SetBool("IsJumping", false);
+        }
+        */
     }
+
+    /*
+    private void FixedUpdate()
+    {
+        if (jump)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(_rayCast.transform.position, Vector2.down);
+            Debug.Log(hit.collider.gameObject.name);
+            Debug.Log(hit.distance);
+
+            //Calculations for raycasting. Should only be when jumping. Origin, Direction, Distance 
+            if(hit.distance == 0.0f)
+            {
+                jump = false;
+                
+            }
+            //animator.SetBool("IsJumping", jump);
+        }
+    }
+    */
 }
